b10cae6d4347110f3ca46f82c2438c7e2cee2fb1
SandroYL <1255417670@qq.com>
2024-06-17 17:34:02 +0800

    a

--- gateway-basic/src/lib.rs
+++ gateway-basic/src/lib.rs
@@ -1,5 +1,7 @@
+use std::fmt;
 use std::error::Error as ErrorTrait;
 
+#[derive(Debug)]
 pub struct Error {
     pub error_type: ErrorType,
     pub error_source: ErrorSource,
@@ -7,7 +9,7 @@ pub struct Error {
     pub error_cause: Option<Box<(dyn ErrorTrait + Send + Sync)>>,
     pub error_desciption: Option<String>,
 }
-
+#[derive(Debug)]
 pub enum ErrorType {
     /*----------Connect Problem------------*/
     ConnectTimeout,
@@ -17,15 +19,25 @@ pub enum ErrorType {
     SocketError,
     HttpCode(u16),
 }
-
+#[derive(Debug)]
 pub enum ErrorSource {
     UpStream,
     DownStream,
     Internal,
     Undefined,
 }
-
+#[derive(Debug)]
 pub enum RetryType {
     Decided(bool),
     ReusedOnly,
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        //链式调用fmt
+        todo!()
+    }
+}
+
+impl ErrorTrait for Error {}
+

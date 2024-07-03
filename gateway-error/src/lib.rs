9e01e753b76d3f7c9e14071185bbf0e6ac4cb0da
SandroYL <1255417670@qq.com>
2024-07-03 18:18:47 +0800

    1

--- gateway-error/src/lib.rs
+++ gateway-error/src/lib.rs
@@ -1,14 +1,50 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fmt::{self};
+use std::error::Error as ErrorTrait;
+
+#[derive(Debug)]
+pub struct Error {
+    pub error_type: ErrorType,
+    pub error_source: ErrorSource,
+    pub error_retry: RetryType,
+    pub error_cause: Option<Box<(dyn ErrorTrait + Send + Sync)>>,
+    pub error_desciption: Option<String>,
+}
+#[derive(Debug)]
+pub enum ErrorType {
+    /*----------Connect Problem------------*/
+    ConnectTimeout,
+    ConnectRefused,
+    /*----------Connect Problem------------*/
+    BindError,
+    SocketError,
+    HttpCode(u16),
+}
+#[derive(Debug)]
+pub enum ErrorSource {
+    UpStream,
+    DownStream,
+    Internal,
+    Undefined,
+}
+#[derive(Debug)]
+pub enum RetryType {
+    Decided(bool),
+    ReusedOnly,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        //链式调用fmt
+        self.chain_display(f)
     }
 }
+
+impl ErrorTrait for Error {}
+
+impl Error {
+    fn chain_display(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "context: {}", self.error_desciption.as_ref().unwrap())
+
+    }
+}
+

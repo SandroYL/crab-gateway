4dee0379b3d0c7316e45ba77d672cb2b20ad6fe3
SandroYL <1255417670@qq.com>
2024-07-03 23:29:08 +0800

    error

--- gateway-basic/Readme.md
+++ gateway-basic/Readme.md
@@ -3,3 +3,5 @@
 ## 功能
 - 允许自定义新的错误类型。需要提供错误名，错误来源，上游错误（可选），错误描述（可选）。
 - 当发生错误时，需要满足：能展示出错误链的层级关系，并追溯到最上层。每一层的错误原因都必须展示。例如：BannedError:此链接被拒绝-->NetError(505):此连接断开。
+
+get-netadapter -IncludeHidden | Format-Table -AutoSize | Where-Object Name -like "vEthernet (WSL (Hyper-V firewall))" | Get-NetAdapterAdvancedProperty -IncludeHidden | Where-Object DisplayName -like "Large Send Offload Version 2*" | Set-NetAdapterAdvancedProperty -DisplayValue Disabled
--- gateway-error/src/lib.rs
+++ gateway-error/src/lib.rs
@@ -9,6 +9,9 @@ pub struct Error {
     pub error_cause: Option<Box<(dyn ErrorTrait + Send + Sync)>>,
     pub error_desciption: Option<String>,
 }
+
+type BErr = Box<Error>;
+
 #[derive(Debug)]
 pub enum ErrorType {
     /*----------Connect Problem------------*/
@@ -18,6 +21,9 @@ pub enum ErrorType {
     BindError,
     SocketError,
     HttpCode(u16),
+    /*----------DIY Problem------------*/
+    Custom(&'static str),
+    CustomCode(&'static str, u16),
 }
 #[derive(Debug)]
 pub enum ErrorSource {
@@ -26,11 +32,21 @@ pub enum ErrorSource {
     Internal,
     Undefined,
 }
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum RetryType {
     Decided(bool),
     ReusedOnly,
 }
+
+impl Into<RetryType> for bool {
+    fn into(self) -> RetryType {
+        match self {
+            true => RetryType::Decided(true),
+            false => RetryType::Decided(false),
+        }
+    }
+}
+
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -39,12 +55,53 @@ impl fmt::Display for Error {
     }
 }
 
+impl Default for Error {
+    fn default() -> Self {
+        Self { 
+            error_type: ErrorType::Custom("null"), 
+            error_source: ErrorSource::DownStream, 
+            error_retry: false.into(), 
+            error_cause: None, 
+            error_desciption: None 
+        }
+    }
+}
+
 impl ErrorTrait for Error {}
 
 impl Error {
     fn chain_display(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "context: {}", self.error_desciption.as_ref().unwrap())
+        write!(f, "context: {}", self.error_desciption.as_ref().unwrap())?;
+        if let Some(case_error) = self.error_cause.as_ref() {
+            write!(f, "\nError is caused by {:?}", case_error.downcast_ref::<Box<Error>>())?;
+            self.chain_display(f)
+        } else {
+            Ok(())
+        }
+    }
 
+    ///generate error with cause.
+    /// 
+    ///[RetryType] not always worked, if error_cause cant retry, then [RetryType] is false.
+    fn generate_error_withcause(error_type: ErrorType, error_source: ErrorSource, 
+        error_retry: RetryType, error_cause: Option<Box<(dyn std::error::Error + Send + Sync + 'static)>>) -> Self {
+        
+        let retry = if let Some(cause) = error_cause.as_ref() {
+            if let Some(upper_cause) = cause.downcast_ref::<Error>() {
+                upper_cause.error_retry
+            } else {
+                false.into()
+            }
+        } else {
+            error_retry
+        };
+        Error {
+            error_type,
+            error_source,
+            error_retry: retry,
+            error_cause,
+            error_desciption: None,
+        }
     }
 }
 

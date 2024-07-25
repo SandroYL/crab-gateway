d3f6f400f7b9927a01e297b6e11a82cd86694ffb
SandroYL <1255417670@qq.com>
2024-07-25 17:25:26 +0800

    1

--- gateway-protocols/src/connections/response.rs
+++ gateway-protocols/src/connections/response.rs
@@ -1,9 +1,9 @@
-use http::{Extensions, HeaderMap, HeaderValue, StatusCode, Version};
+use http::{Extensions, HeaderMap, HeaderValue, Result, StatusCode, Version};
 
 
 
 pub struct Response<T> {
-    head: String,
+    head: ResponseHeader,
     body: T,
 }
 
@@ -12,5 +12,63 @@ pub struct ResponseHeader {
     pub version: Version,
     pub headers: HeaderMap<HeaderValue>,
     pub extensions: Extensions,
-    _priv: (),
+    _priv: (), //防止外界直接构建请求
+}
+
+pub struct ResponseHeaderBuilder {
+    inner: Result<ResponseHeader>
+}
+
+impl ResponseHeader {
+    pub fn new() -> Self {
+        let mut p = HeaderMap::new();
+        Self {
+            status: StatusCode::default(),
+            version: Version::default(),
+            headers: HeaderMap::default(),
+            extensions: Extensions::default(),
+            _priv: (),
+        }
+    }
+}
+
+impl<T> Response<T> {
+    #[inline]
+    pub fn builder() -> ResponseHeaderBuilder {
+        ResponseHeaderBuilder::new()
+    }
+
+    #[inline]
+    pub fn new(body: T) -> Response<T> {
+        Response {
+            head: ResponseHeader::new(),
+            body,
+        }
+    }
+
+    //lose response forever
+    pub fn divide_response(self) -> (ResponseHeader, T) {
+        (self.head, self.body)
+    }
+
+    pub fn union_response(headers: ResponseHeader, body: T) -> Self {
+        Self {
+            head: headers,
+            body,
+        }
+    }
+}
+
+impl ResponseHeaderBuilder {
+    pub fn new() -> Self {
+        ResponseHeaderBuilder::default()
+    }
+}
+
+impl Default for ResponseHeaderBuilder {
+    fn default() -> Self {
+        Self {
+            inner: Ok(ResponseHeader::new()),
+        }
+    }
 }

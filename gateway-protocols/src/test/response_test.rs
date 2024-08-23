a9f7a9a3288c7e105ffcc4984152251d6ee737a6
SandroYL <1255417670@qq.com>
2024-08-23 17:26:33 +0800

    1

--- gateway-protocols/src/test/response_test.rs
+++ gateway-protocols/src/test/response_test.rs
@@ -20,42 +20,4 @@ mod tests {
         let req = ResponseHeader::build_with_status_code("999");
         assert!(req.is_err());
     }
-/*
-    #[test]
-    fn test_modify_header() {
-        let mut req = ResponseHeader::build_with_method_path("GET", b"/icbc/biom").unwrap();
-
-        req.insert_header("foo", "bar").unwrap();
-        req.append_header("Foo", "fkv").unwrap();
-        req.modify_header("FoO", "shit").unwrap();
-        let mut buf = vec![];
-        req.header_to_h1_wire(&mut buf);
-        assert_eq!(buf, b"foo: shit\r\n");  
-    }
-
-    #[test]
-    fn test_remove_header() {
-        let mut req = ResponseHeader::build_with_method_path("GET", b"/icbc/biom").unwrap();
-
-        req.insert_header("foo", "bar").unwrap();
-        req.append_header("Foo", "fkv").unwrap();
-        req.append_header("vio", "shit").unwrap();
-        req.remove_header("foo", "shit").unwrap();
-        let mut buf = vec![];
-        req.header_to_h1_wire(&mut buf);
-        assert_eq!(buf, b"vio: shit\r\n");  
-    }
-
-    #[test]
-    fn test_format_header() {
-        let mut req = ResponseHeader::build_with_method_path("GET", b"/icbc/biom").unwrap();
-
-        req.insert_header("foo", "bar").unwrap();
-        req.insert_header(http::header::CONTENT_TYPE, "piece").unwrap();
-        req.insert_header(http::header::CONTENT_TYPE, "down").unwrap();
-        let mut buf = vec![];
-        req.header_to_h1_wire(&mut buf);
-        assert_eq!(buf, b"foo: bar\r\ncontent-type: down\r\n");          
-    }
-*/
 }
